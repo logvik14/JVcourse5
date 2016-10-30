d96cb9130c9d68a03c99fe05f0448d43e8b5a120
logvik14 <vladimirlogvinov23@gmail.com>
2016-10-30 18:04:41 +0200

    init commit

---
+++ src/Test.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Vladymyr on 26.10.2016.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test...");
+
+        System.out.println("Vovan");
+
+        System.out.println("Run away");
+    }
+}
